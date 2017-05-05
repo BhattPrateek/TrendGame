01620cddc579ba9b5dd2b223cddac537b425f2b6
bhattprateek <prateekbhatt94@gmail.com>
2017-05-05 16:48:03 -0700

    (feat)Add chart properties to chart component.

--- client/app/components/Chart.jsx
+++ client/app/components/Chart.jsx
@@ -1,9 +1,26 @@
 import React from 'react';
+import { Chart } from 'react-google-charts'
 
 export default class Chart extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      options:{
+        title: 'Trend Popularity Graph'
+        hAxis: 
+        vAxis:
+        legend:
+      }
+    }
+  }
   render () {
     return (
-      <input placeholder="CHART PLACEHOLDER"></input>
+      <div>
+        <Chart
+          chartType="LineChart"
+          data= {[], }
+        />
+      </div>
     );
   }
 }
