930dd152d7329a2fedb5d8e3da28e2cd46982d4a
bhattprateek <prateekbhatt94@gmail.com>
2017-05-10 15:05:17 -0700

    (feat)Integrate user input to do a get request to effect chart data.

--- client/app/index.jsx
+++ client/app/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import {render} from 'react-dom';
+import axios from 'axios';
 import data from '../../integration-tests/fixtures/stitched-timeline';
 import Layout from './components/Layout';
 
@@ -22,19 +23,20 @@ class App extends React.Component {
         q: trend
       }
      })
-    .then(function (response) {
-      console.log(response);
+    .then( response => {
+      console.log("This is the reponse!", response.data);
       var dataTuple = [];
       dataTuple.push(['Date', 'Popularity']);
-      for (var i = 0; i < data.length; i++) {
-        dataTuple.push( [data[i].date, data[i].popularity] );
+      for (var i = 0; i < response.data.length; i++) {
+        dataTuple.push( [response.data[i].date, response.data[i].popularity] );
         if (i === 0) {
-          this.setState({start: data[i].date});
+          this.setState({start: response.data[i].date});
         }
-        if (i === data.length - 1) {
-          this.setState({end: data[i].date});
+        if (i === response.data.length - 1) {
+          this.setState({end: response.data[i].date});
         }
       }
+      console.log("This is the dataTuple", dataTuple);
       this.setState({data: dataTuple});
       console.log(this.state.start, this.state.end, this.state.data);
     })
